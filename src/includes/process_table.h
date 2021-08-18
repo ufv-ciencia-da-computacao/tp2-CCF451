cdc260e564d2dda0a9a8946eb632099a7d40c487
Dener Vieira <deneribeiro10@hotmail.com>
2021-08-18 17:35:41 -0300

    add process table header

--- src/includes/process_table.h
+++ src/includes/process_table.h
@@ -1,6 +1,15 @@
 #ifndef __PROCESS_TABLE_H__
 #define __PROCESS_TABLE_H__
 
-typedef struct process_table process_table_t;
+typedef struct process_table * process_table_t;
+
+typedef enum {
+    state_ready,
+    state_executing,
+    state_blocked
+} process_state;
+
+void process_table_init(process_table_t *pt, int size);
+void process_table_destroy(process_table_t *pt);
 
 #endif
--- src/process_table.c
+++ src/process_table.c
@@ -1,7 +1,35 @@
 #include "./includes/process_table.h"
-#include "./includes/process.h"
+#include "./time.h"
+#include <stdlib.h>
 
 struct process_table {
-    process_t *list;
+    struct {
+        int pid;
+        int parent_pid;
+        int program_counter;
+        int *data_memory_ptr;
+        int priority;
+        process_state state;
+        time_t begin_time;
+        time_t used_time;
+    } *list;
     int size;
 };
+
+void process_table_init(process_table_t *pt, int size) {
+    int i;
+    (*pt) = malloc(sizeof **pt);
+    (*pt)->size = size;
+    (*pt)->list = malloc(size * sizeof(*(*pt)->list));
+    for(i=0; i<size; ++i) {
+        (*pt)->list[i].program_counter = 0;
+        (*pt)->list[i].pid = -1;
+        /* initialize all values */
+    }
+}
+
+void process_table_destroy(process_table_t *pt) {
+    free((*pt)->list);
+    free(*pt);
+    *pt = NULL;
+}
