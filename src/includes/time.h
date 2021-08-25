b3862b2f42b2897e3881944070d2f8a6a6760ef3
Dener Vieira <deneribeiro10@hotmail.com>
2021-08-25 11:58:12 -0300

    change time structure

--- src/includes/time.h
+++ src/includes/time.h
@@ -1,10 +1,8 @@
 #ifndef __TIME_H__
 #define __TIME_H__
 
-typedef struct time * time_t;
+typedef int time_t;
 
 void time_init(time_t *time);
-void time_tick(time_t *time);
-int  time_now(time_t *time);
 
 #endif
--- src/time.c
+++ src/time.c
@@ -1,19 +1,6 @@
 #include "./includes/time.h"
 #include <stdlib.h>
 
-struct time {
-    int t;
-};
-
 void time_init(time_t *time) {
-    *time = malloc(sizeof **time);
-    (*time)->t = 0;
-}
-
-void time_tick(time_t *time) {
-    (*time)->t++;
-}
-
-int  time_now(time_t *time) {
-    return (*time)->t;
+    (*time) = 0;
 }
