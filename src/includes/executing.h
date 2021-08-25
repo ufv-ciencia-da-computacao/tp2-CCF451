ec58f59637bca32baa0834c2b9b7c46099133b19
gegen07 <gegenbarcelos@gmail.com>
2021-08-25 14:32:38 -0300

    chore: add executing operations struct

--- src/includes/executing.h
+++ src/includes/executing.h
@@ -3,8 +3,15 @@
 
 typedef struct executing * executing_t;
 
+struct executing {
+    // ids da tabela de processos
+    int id;
+    int working;
+};
+
 void executing_init(executing_t *executing);
 void executing_destroy(executing_t *executing);
-void executing_set(executing_t *executing /*, parameters */);
+void executing_set(executing_t *executing, int id);
+int executing_get(executing_t *executing);
 
 #endif
