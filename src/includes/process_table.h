3218cd0b2d760d19ca1060ea07d81379e0fafe42
Dener Vieira <deneribeiro10@hotmail.com>
2021-09-02 17:04:53 -0300

    add process table

--- src/includes/process_table.h
+++ src/includes/process_table.h
@@ -2,8 +2,8 @@
 #define __PROCESS_TABLE_H__
 
 #include "time.h"
-
-#define PID_MAX 32768
+#include "program.h"
+#include "data.h"
 
 typedef enum {
     state_ready,
@@ -13,24 +13,27 @@ typedef enum {
 
 typedef struct {
     struct {
-        int pid;
-        int parent_pid;
-        int program_counter;
-        // *program_ptr
-        int *data_memory_ptr;
-        int priority;
-        process_state state;
-        time_t begin_time;
-        time_t used_time;
-    } data[PID_MAX+1];
-    int available[PID_MAX+1];
+        struct {
+            int pid;
+            int parent_pid;
+            int program_counter;
+            program_t program;
+            data_t data;
+            int priority;
+            process_state state;
+            time_t begin_time;
+            time_t used_time;
+        } process;
+        int available;
+    } *data;
+    int size;
+    int next_index;
 } process_table_t;
 
-void process_table_init(process_table_t *pt);
+void process_table_init(process_table_t *pt, int size);
 void process_table_destroy(process_table_t *pt);
-int  process_table_add(process_table_t *pt, int parent_pid, int program_counter, int *data_memory_ptr, int priority, time_t now);
-void process_table_update_process(process_table_t *pt /*, parameters */);
-void process_table_get_priority(process_table_t *pt, int index);
+int process_table_add(process_table_t *pt, int parent_pid, int program_counter, program_t program, data_t data, int priority, time_t begin_time);
+void process_table_update(process_table_t *pt, int index, int program_counter, program_t program, data_t data, int priority, process_state state, time_t used_time);
 void process_table_remove(process_table_t *pt, int index);
 
 #endif
--- src/process_table.c
+++ src/process_table.c
@@ -1,47 +1,62 @@
 #include "./includes/process_table.h"
 #include <stdlib.h>
 
-static int next_pid = 0;
-
-void process_table_init(process_table_t *pt) {
+void process_table_init(process_table_t *pt, int size) {
+    pt->data = malloc(size * sizeof(*pt->data));
+    pt->size = size;
+    pt->next_index = 0;
     int i;
-    for(i=0; i<=PID_MAX; ++i) {
-        pt->available[i] = 1;
+    for(i=0; i<pt->size; ++i) {
+        pt->data[i].available = 1;
     }
 }
 
 void process_table_destroy(process_table_t *pt) {
-    
+    int i;
+    for(i=0; i<pt->size; ++i) {
+        program_destroy(&pt->data[i].process.program);
+        data_destroy(&pt->data[i].process.data);
+    }
+    free(pt->data);
+    pt->data = NULL;
+    pt->size = 0;
 }
 
-int process_table_add(process_table_t *pt, int parent_pid, int program_counter, int *data_memory_ptr, int priority, time_t now) {
+int process_table_add(process_table_t *pt, int parent_pid, int program_counter, program_t program, data_t data, int priority, time_t begin_time) {
     /*find first available position and add this process */
     int i;
-    int pid = -1;
-    for(i=0; i <= PID_MAX && pid == -1; ++i, next_pid = (next_pid == PID_MAX) ? 0 : next_pid+1) {
-        if(pt->available[next_pid]) {
-            pt->available[next_pid] = 0;
-            pt->data[next_pid].pid = next_pid;
-            pt->data[next_pid].parent_pid = parent_pid;
-            pt->data[next_pid].program_counter = program_counter;
-            // init program memory
-            pt->data[next_pid].data_memory_ptr = data_memory_ptr;
-            pt->data[next_pid].priority = priority;
-            pt->data[next_pid].used_time = 0;
-            pt->data[next_pid].begin_time = now;
-            
-            pid = next_pid;
+    int index = -1;
+    for(i=0; i < pt->size && index == -1; ++i, pt->next_index = (pt->next_index == pt->size-1) ? 0 : pt->next_index+1) {
+        if(pt->data[pt->next_index].available) {
+            index = pt->next_index;
+            pt->data[index].available = 0;
+            pt->data[index].process.pid = index;
+            pt->data[index].process.parent_pid = parent_pid;
+            pt->data[index].process.program_counter = program_counter;
+            pt->data[index].process.program = program;
+            pt->data[index].process.data = data;
+            pt->data[index].process.priority = priority;
+            pt->data[index].process.state = state_ready;
+            pt->data[index].process.begin_time = begin_time;
+            pt->data[index].process.used_time = 0;
         }
     }
 
-    return pid;
+    return index;
 }
 
-void process_table_update(process_table_t *pt /*, parameters */);
-void process_table_get_priority(process_table_t *pt, int index);
+void process_table_update(process_table_t *pt, int index, int program_counter, program_t program, data_t data, int priority, process_state state, time_t used_time) {
+    pt->data[index].process.program_counter = program_counter;
+    program_destroy(&pt->data[index].process.program);
+    pt->data[index].process.program = program_copy(&program);
+    data_copy(&pt->data[index].process.data, data);
+    pt->data[index].process.priority = priority;
+    pt->data[index].process.state = state;
+    pt->data[index].process.used_time = used_time;
+}
 
 void process_table_remove(process_table_t *pt, int index) {
-    pt->available[index] = 1;
-    // free(pt->data[index].data_memory_ptr);
-    // free program memory ptr
+    pt->data[index].available = 1;
+    data_destroy(&pt->data[index].process.data);
+    program_destroy(&pt->data[index].process.program);
 }
